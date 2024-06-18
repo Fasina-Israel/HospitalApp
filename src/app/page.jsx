7c0ef6bc209026c5a985611009af564bd61f73ff
Fasina-Israel <fashman28@gmail.com>
2024-06-18 22:05:18 +0100

    removing redux store

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,14 @@
 'use client'
 import React from 'react';
-import { store } from './redux/store';
-import { Provider } from 'react-redux';
 import CounterPage from './redux/page'
 import Register from './register/page';
 
 const Home = () => {
   return (
-    <Provider store={store}>
+    <>
       <Register />
-    </Provider>
+    </>
+
 
   )
 }
