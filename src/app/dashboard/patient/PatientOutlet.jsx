75ba4b2d0d01f003195f4937a7151824ab0b5125
Fasina-Israel <fashman28@gmail.com>
2024-06-18 17:38:23 +0100

    fixing the patience dashboard

--- src/app/dashboard/patient/PatientOutlet.jsx
+++ src/app/dashboard/patient/PatientOutlet.jsx
@@ -38,7 +38,7 @@ const PatientOutlet = () => {
     return (
         <Box sx={{
             width: '100%',
-            height: '100%',
+            height: '90%',
             backgroundColor: '#fff',
             justifyContent: 'flexStart',
             alignItems: 'center'
