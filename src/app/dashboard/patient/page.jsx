b8c06e0eae58740e92ee4b6df50bb38414c7b013
Fasina-Israel <fashman28@gmail.com>
2024-06-19 02:59:43 +0100

    added useEffect

--- src/app/dashboard/patient/page.jsx
+++ src/app/dashboard/patient/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Box, Text, Stack, IconButton } from '@mui/material'
 // import { Outlet } from 'react-router-dom'
 import { useTheme } from '@mui/material/styles';
@@ -16,37 +16,42 @@ const Page = () => {
   console.log(token, 'token')
 
   const theme = useTheme()
-  if (token) {
-    return (
-      <>
-        <Box display={'flex'} flexDirection={'row'}>
-          <Box p={2} sx={{ backgroundColor: theme.palette.background.paper, boxShadow: '0px 0px 2px rgba(0,0,0,0.25)', height: '100vh', width: 100 }}>
-            <Stack direction="column" alignItems={"center"} sx={{ width: '100%' }} spacing={2} >
-              <Box sx={{
-                backgroundColor: theme.palette.primary.main,
-                height: 64,
-                width: 64,
-                borderRadius: 1.5
-              }}>
-              </Box>
-              <Stack spacing={3}>
-                {Nav_Buttons.map((el) => <IconButton key={el.index}>
-                  {el.icon}
-                </IconButton>)}
+  const loadPage = () => {
+    if (token) {
+      return (
+        <>
+          <Box display={'flex'} flexDirection={'row'}>
+            <Box p={2} sx={{ backgroundColor: theme.palette.background.paper, boxShadow: '0px 0px 2px rgba(0,0,0,0.25)', height: '100vh', width: 100 }}>
+              <Stack direction="column" alignItems={"center"} sx={{ width: '100%' }} spacing={2} >
+                <Box sx={{
+                  backgroundColor: theme.palette.primary.main,
+                  height: 64,
+                  width: 64,
+                  borderRadius: 1.5
+                }}>
+                </Box>
+                <Stack spacing={3}>
+                  {Nav_Buttons.map((el) => <IconButton key={el.index}>
+                    {el.icon}
+                  </IconButton>)}
+                </Stack>
               </Stack>
-            </Stack>
+            </Box>
+            {/* <Outlet /> */}
+            <Box p={2} sx={{ backgroundColor: theme.palette.background.paper, boxShadow: '0px 0px 2px rgba(0,0,0,0.25)', height: '100vh', width: 1400 }}>
+              <PatientOutlet />
+            </Box>
           </Box>
-          {/* <Outlet /> */}
-          <Box p={2} sx={{ backgroundColor: theme.palette.background.paper, boxShadow: '0px 0px 2px rgba(0,0,0,0.25)', height: '100vh', width: 1400 }}>
-            <PatientOutlet />
-          </Box>
-        </Box>
-      </>
-    )
+        </>
+      )
+    }
+    else {
+      router.push('/login');
+    }
   }
-  else {
-    router.push('/login');
-  }
+  useEffect(() => {
+    loadPage()
+  }, []);
 }
 
 export default Page;
