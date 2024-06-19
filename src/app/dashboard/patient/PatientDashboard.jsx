df22ee0c18591c24c7e96c7b7bf9d485a7e118de
Fasina-Israel <fashman28@gmail.com>
2024-06-19 02:33:04 +0100

    fixing window problem

--- src/app/dashboard/patient/PatientDashboard.jsx
+++ src/app/dashboard/patient/PatientDashboard.jsx
@@ -6,36 +6,47 @@ import { Box, Text, Stack, IconButton } from '@mui/material'
 import { useTheme } from '@mui/material/styles';
 import { Nav_Buttons } from '../../data';
 import PatientOutlet from './PatientOutlet';
+import { useRouter } from 'next/navigation'
 
 const PatentDashboard = () => {
+    const router = useRouter()
+    const parsedObj = JSON.parse(window.sessionStorage.getItem('auth'));
+    console.log(parsedObj, 'pObj')
+    const token = JSON.parse(window.sessionStorage.getItem('token'));
+    console.log(token, 'token')
 
     const theme = useTheme()
-    return (
-        <>
-            <Box display={'flex'} flexDirection={'row'}>
-                <Box p={2} sx={{ backgroundColor: theme.palette.background.paper, boxShadow: '0px 0px 2px rgba(0,0,0,0.25)', height: '100vh', width: 100 }}>
-                    <Stack direction="column" alignItems={"center"} sx={{ width: '100%' }} spacing={2} >
-                        <Box sx={{
-                            backgroundColor: theme.palette.primary.main,
-                            height: 64,
-                            width: 64,
-                            borderRadius: 1.5
-                        }}>
-                        </Box>
-                        <Stack spacing={3}>
-                            {Nav_Buttons.map((el) => <IconButton key={el.index}>
-                                {el.icon}
-                            </IconButton>)}
+    if (token) {
+        return (
+            <>
+                <Box display={'flex'} flexDirection={'row'}>
+                    <Box p={2} sx={{ backgroundColor: theme.palette.background.paper, boxShadow: '0px 0px 2px rgba(0,0,0,0.25)', height: '100vh', width: 100 }}>
+                        <Stack direction="column" alignItems={"center"} sx={{ width: '100%' }} spacing={2} >
+                            <Box sx={{
+                                backgroundColor: theme.palette.primary.main,
+                                height: 64,
+                                width: 64,
+                                borderRadius: 1.5
+                            }}>
+                            </Box>
+                            <Stack spacing={3}>
+                                {Nav_Buttons.map((el) => <IconButton key={el.index}>
+                                    {el.icon}
+                                </IconButton>)}
+                            </Stack>
                         </Stack>
-                    </Stack>
+                    </Box>
+                    {/* <Outlet /> */}
+                    <Box p={2} sx={{ backgroundColor: theme.palette.background.paper, boxShadow: '0px 0px 2px rgba(0,0,0,0.25)', height: '100vh', width: 1400 }}>
+                        <PatientOutlet />
+                    </Box>
                 </Box>
-                {/* <Outlet /> */}
-                <Box p={2} sx={{ backgroundColor: theme.palette.background.paper, boxShadow: '0px 0px 2px rgba(0,0,0,0.25)', height: '100vh', width: 1400 }}>
-                    <PatientOutlet />
-                </Box>
-            </Box>
-        </>
-    )
+            </>
+        )
+    }
+    else {
+        router.push('/login');
+    }
 }
 
 export default PatentDashboard;
--- src/app/dashboard/patient/page.jsx
+++ src/app/dashboard/patient/page.jsx
@@ -1,24 +1,14 @@
 "use client"
 import React from 'react';
 import Dashboard from './PatientDashboard';
-import { useRouter } from 'next/navigation'
 
 const Page = () => {
-  const router = useRouter()
-  const parsedObj = JSON.parse(window.sessionStorage.getItem('auth'));
-  console.log(parsedObj, 'pObj')
-  const token = JSON.parse(window.sessionStorage.getItem('token'));
-  console.log(token, 'token')
-  if (token) {
-    return (
-      <div>
-        <Dashboard />
-      </div>
-    )
-  }
-  else {
-    router.push('/login');
-  }
+
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  )
 }
 
 export default Page
