a3c8acc4352ba03bc70b3c9d237341b3b888d00d
Fasina-Israel <fashman28@gmail.com>
2024-06-19 02:14:09 +0100

    implemented protected route

--- src/app/dashboard/patient/page.jsx
+++ src/app/dashboard/patient/page.jsx
@@ -1,12 +1,24 @@
+"use client"
 import React from 'react';
 import Dashboard from './PatientDashboard';
+import { useRouter } from 'next/navigation'
 
 const page = () => {
-  return (
-    <div>
-      <Dashboard />
-    </div>
-  )
+  const router = useRouter()
+  const parsedObj = JSON.parse(window.sessionStorage.getItem('auth'));
+  console.log(parsedObj, 'pObj')
+  const token = JSON.parse(window.sessionStorage.getItem('token'));
+  console.log(token, 'token')
+  if (token) {
+    return (
+      <div>
+        <Dashboard />
+      </div>
+    )
+  }
+  else {
+    router.push('/login');
+  }
 }
 
 export default page
